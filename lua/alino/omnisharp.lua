1ca3dd2158fb909e4dad40fa6fdb73cc86054f92
Alexander Sadovsky <alino.sadovsky@gmail.com>
2024-07-23 20:11:09 +0200

    update to use lazy

--- lua/alino/omnisharp.lua
+++
@@ -1,8 +0,0 @@
-local pid = vim.fn.getpid()
-
-local omnisharp_bin = "/usr/local/bin/omnisharp-roslyn/OmniSharp"
-
-require'lspconfig'.omnisharp.setup{
-    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) }
-    -- Additional configuration can be added here
-}
--- lua/alino/remap.lua
+++ lua/alino/remap.lua
@@ -3,10 +3,10 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
 
 -- Remap Ctrl+A to go to the beginning of the line in normal mode
-vim.keymap.set({"n"}, "<C-a>", "0")
+-- vim.keymap.set({"n"}, "<C-a>", "0")
 
 -- Remap Ctrl+E to go to the end of the line in normal mode
-vim.keymap.set({"n"}, "<C-e>", "$")
+-- vim.keymap.set({"n"}, "<C-e>", "$")
 
 -- Remap Ctrl+A to go to the beginning of the line in insert mode
 vim.keymap.set("i", "<C-a>", "<Esc>^i", {noremap = true})
@@ -23,44 +23,69 @@ vim.keymap.set("i", "<C-e>", "<Esc>$a", {noremap = true})
 -- Use H for insert instead of I
 -- vim.api.nvim_set_keymap('n', 'h', 'i', {noremap = true})
 
-local opts = { noremap = true, expr = true, silent = true }
--- because or opts.expr we gotta return something
-vim.keymap.set("i", "<Tab>", function()
-  if vim.fn["coc#pum#visible"]() == 1 then
-    return vim.fn["coc#pum#next"](1)
-  --elseif vim.fn["coc#expandableOrJumpable"]() then
-    --return "<Plug>(coc-snippets-expand-jump)"
-  else
-    return "<C-i>"
-  end
-end, opts)
 
-vim.keymap.set("i", "<S-Tab>", function()
-  if vim.fn["coc#pum#visible"]() == 1 then
-    return vim.fn["coc#pum#prev"](1)
-  elseif vim.fn["coc#jumpable"]() then
-    return vim.fn["coc#snippet#prev"]() --> this turns into <Ignore> when the keymap runs
-  elseif vim.fn.indent(vim.fn.line(".")) == vim.fn.col(".") - 1 then
-    return "<C-d>"
-  else
-    return "<C-w>"
-  end
-end, opts)
 
-vim.keymap.set("i", "<CR>", function()
-  if vim.fn["coc#pum#visible"]() == 1 then
-    return vim.fn["coc#pum#confirm"]()
-  else
-    return "<CR>"
-  end
-end, opts)
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set('n', 'n', 'nzzzv', { noremap = true, silent = true })
+vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true })
 
--- This might be useful since some snippets sometimes add text before the
--- cursor and that might influence completion
-vim.keymap.set("i", "<C-Space>", function()
-  if vim.fn["coc#pum#visible"]() == 1 then
-    vim.fn["coc#pum#close"]()
-  else
-    vim.fn["coc#start"]()
-  end
-end, { noremap = true, silent = true })
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Move cursor to vertical center on scroll
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+vim.keymap.set("n", "<leader>vwm", function()
+    require("vim-with-me").StartVimWithMe()
+end)
+vim.keymap.set("n", "<leader>svwm", function()
+    require("vim-with-me").StopVimWithMe()
+end)
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
+
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
